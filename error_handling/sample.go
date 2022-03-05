6798578e5324a01e79b05388aac714935eb0b497
daikidev111 <dkub0001@student.monash.edu>
2022-03-05 16:02:50 +0900

    Add a test case

--- error_handling/sample.go
+++ error_handling/sample.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	errorCh := make(chan error)
-	go func(ch chan error) {
+	go func(ch chan error) { // want "identifier is goroutine"
 		ch <- errors.New("error")
 	}(errorCh)
 
